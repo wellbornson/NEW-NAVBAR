974e76e80e7c3ba314a2c98bc7973cfb7b001f35
wellbornson007 <wellbornson@yahoo.com>
2024-12-16 22:05:28 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home(){
 <div>
 Class 3  Assignment: 
 <br/>
-Practice the concepts you've learned in class through hands-on exercises.
+Practice the concepts you&apos;ve learned in class through hands-on exercises.
 <br/>
 Create a footer and render it once so that it's available across all routes.
 <br/><br/><br/><br/>
